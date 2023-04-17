4cc54a70baeef46d66c5c2a2928adb824083e0ed
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2023-04-17 14:49:26 +0300

    chore: empty project

---
+++ src/test/java/com/coding/like/mozart/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.like.mozart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
